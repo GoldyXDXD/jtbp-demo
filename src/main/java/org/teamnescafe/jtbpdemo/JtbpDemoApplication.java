af7b8e31f31eb55fa1cca8533663f2df625b6163
GoldyXDXD <62290862+GoldyXDXD@users.noreply.github.com>
2021-09-10 19:00:59 +0300

    Telegram Bot is created

---
+++ src/main/java/org/teamnescafe/jtbpdemo/JtbpDemoApplication.java
@@ -0,0 +1,13 @@
+package org.teamnescafe.jtbpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtbpDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtbpDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/teamnescafe/jtbpdemo/JtbpDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.teamnescafe.jtbpdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtbpDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
