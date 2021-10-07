e001988c9860bd6c79eea36fb0cc10325a862121
GoldyXDXD <62290862+GoldyXDXD@users.noreply.github.com>
2021-10-07 23:59:26 +0300

    - в проект добавлено расписание группы

---
+++ src/main/java/org/teamnescafe/jtbpdemo/Calendar.java
@@ -0,0 +1,14 @@
+package org.teamnescafe.jtbpdemo;
+
+import net.fortuna.ical4j.model.Calendar;
+
+class GroupTimetable {
+
+    /*private final Calendar timetable;
+
+    public GroupTimetable(Calendar timetable) {
+        this.timetable = timetable;
+    }
+    */
+
+}
